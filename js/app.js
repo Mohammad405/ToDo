ade9004c16e661412d897a69a8a2baf4538d5212
Mohammad Azawi <mohammadazawi1@gmail.com>
2020-06-04 17:09:51 -0400

    Add <li>, <span> to document function

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const addItem = document.querySelector('#item');
+
+const addButton = document.querySelector('#bt');
+
+const ul = document.querySelector('#list ul');
+
+addButton.addEventListener('click', () => {
+    let li = document.createElement('li');
+    let sp = del();
+    li.textContent = addItem.value;
+    li.appendChild(sp);
+    ul.appendChild(li);
+    addItem.value = '';
+});
+
+const del = () =>{
+    let span = document.createElement('span');
+    let img = document.createElement('img');
+    img.setAttribute('src',"img/close.svg");
+    img.setAttribute('alt',"exit icon");
+    img.classList.add('l-img')
+    span.appendChild(img);
+    return span;
+}
