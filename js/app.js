3474c69e7d224bf4895a1f637113138e5e310acd
Mohammad Azawi <mohammadazawi1@gmail.com>
2020-06-04 17:18:39 -0400

    Add empty input logic

--- js/app.js
+++ js/app.js
@@ -8,6 +8,15 @@ addButton.addEventListener('click', () => {
     let li = document.createElement('li');
     let sp = del();
     li.textContent = addItem.value;
+
+    if(addItem.value === ''){
+        addItem.classList.add('red');
+        alert("Try agian");
+        li.document.removeChild();
+    }else{
+        addItem.classList.remove('red');
+    }
+
     li.appendChild(sp);
     ul.appendChild(li);
     addItem.value = '';
